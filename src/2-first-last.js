42cfa5d244287ba9546c7dd98c48489eb64ceb88
Agnes Ketesdi <agnes@Agness-MacBook-Air.local>
2019-11-19 22:22:08 +0100

    first commit

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,3 +1,10 @@
-export const firstLast = (items) => {
-  return `First: ${items[0]}, Last: ${items[1]}`
-}
+export const firstLast = items => {
+  if (items.length === 0) {
+    return "No items!";
+  }
+  if (items.length === 1) {
+    return `Only item: ${item[0]}`;
+  }
+
+  return `First: ${items[0]}, Last: ${items[items.length - 1]}`;
+};
